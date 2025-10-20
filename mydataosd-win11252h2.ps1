e5bfed5b38610c54c5fcd0dc13f16026594283fe
mydata-it <153841125+mydata-it@users.noreply.github.com>
2025-10-20 10:38:57 +0200

    Add files via upload

---
+++ mydataosd-win11252h2.ps1
@@ -0,0 +1,12 @@
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail -ZTI
+
+#Restart from WinPE
+
+Write-Host -ForegroundColor Green Restarting in 20 seconds!
+
+Start-Sleep -Seconds 20
+
+wpeutil reboot
